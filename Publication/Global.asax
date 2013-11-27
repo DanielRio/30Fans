4474b501ab4b41d2109cefaf2ed22a9e92600cfa
Joao Paulo Pozo Lindgren <jplindgrenbr@hotmail.com>
2013-11-27 00:55:33 -0200

    Cleaning, facebook button, twitter button

--- Publication/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_30Fans.MvcApplication" Language="C#" %>
--- scripts/Create Products.sql
+++ scripts/Create Products.sql
@@ -6,55 +6,55 @@ INSERT INTO `jplindgren02`.`Product`
 VALUES('Botafogo','botafogo','png',30,'59PUG9GE5UZBJ',Now(),2, 1,10);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Fluminense','fluminense','png',30,'G4ENK8T2MZFJ2',Now(),2, 1,10);
+VALUES('Fluminense','fluminense','png',30,'E2W7TUXUGEX7S',Now(),2, 1,10);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Vasco','vasco','png',30,'59PUG9GE5UZBJ',Now(),2, 1,10);
+VALUES('Vasco','vasco','png',30,'5RCNDNNNQRA7E',Now(),2, 1,10);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Corinthians','corintians','png',30,'59PUG9GE5UZBJ',Now(),2,1,9);
+VALUES('Corinthians','corintians','png',30,'NTAYCU6AXKQMC',Now(),2,1,9);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Palmeiras','palmeiras','png',30,'59PUG9GE5UZBJ',Now(),2,1,9);
+VALUES('Palmeiras','palmeiras','png',30,'ENDMASKZ2PYLU',Now(),2,1,9);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('São Paulo','sao_paulo','png',30,'59PUG9GE5UZBJ',Now(),2,1,9);
+VALUES('São Paulo','sao_paulo','png',30,'77J84JP5BJZBE',Now(),2,1,9);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Santos','santos','png',30,'59PUG9GE5UZBJ',Now(),2,1,9);
+VALUES('Santos','santos','png',30,'YC4WCRQ3QBP4W',Now(),2,1,9);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('América','america_mineiro','png',30,'59PUG9GE5UZBJ',Now(),2,1,8);
+VALUES('América','america_mineiro','png',30,'8F469FD2TD634',Now(),2,1,8);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Atlético Mineiro','atletico_mineiro','png',30,'59PUG9GE5UZBJ',Now(),2,1,8);
+VALUES('Atlético Mineiro','atletico_mineiro','png',30,'R4MAG5W5YGFM2',Now(),2,1,8);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Cruzeiro','cruzeiro','png',30,'59PUG9GE5UZBJ',Now(),2,1,8);
+VALUES('Cruzeiro','cruzeiro','png',30,'XNYYY46YU2L48',Now(),2,1,8);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Altético Goianiense','atletico_goianiense','png',30,'59PUG9GE5UZBJ',Now(),2,1,8);
+VALUES('Altético Goianiense','atletico_goianiense','png',30,'62KBW3EL3C9ZQ',Now(),2,1,8);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Altético Paranaense','atletico_paranaense','png',30,'59PUG9GE5UZBJ',Now(),2,1,7);
+VALUES('Altético Paranaense','atletico_paranaense','png',30,'HF2PQ56AEH2JQ',Now(),2,1,7);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Avaí','avai','png',30,'59PUG9GE5UZBJ',Now(),2,1,7);
+VALUES('Avaí','avai','png',30,'HSYNXM73RP7VS',Now(),2,1,7);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Internacional','internacional','png',30,'59PUG9GE5UZBJ',Now(),2,1,7);
+VALUES('Internacional','internacional','png',30,'YRKU6FMY8WBES',Now(),2,1,7);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Gremio','gremio','png',30,'59PUG9GE5UZBJ',Now(),2,1,7);
+VALUES('Gremio','gremio','png',30,'SXCZSWUN6NS9G',Now(),2,1,7);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Bahia','bahia','png',30,'59PUG9GE5UZBJ',Now(),2,1,6);
+VALUES('Bahia','bahia','png',30,'KBVW8AGBFDVR8',Now(),2,1,6);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Ceará','ceara','png',30,'59PUG9GE5UZBJ',Now(),2,1,6);
+VALUES('Ceará','ceara','png',30,'KQD5NSYZSW64W',Now(),2,1,6);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Coritiba','coritiba','png',30,'59PUG9GE5UZBJ',Now(),2,1,6);
+VALUES('Coritiba','coritiba','png',30,'CN3QN97QG63AQ',Now(),2,1,6);
 INSERT INTO `jplindgren02`.`Product`
 (`ProductName`, `ImageName`, `ImageExtension`,`AvailableQuantity`,`PaymentCode`,`PublishedDate`,`CategoryItemId`,`Enable`, `Priority`)
-VALUES('Figueirense','figueirense','png',30,'59PUG9GE5UZBJ',Now(),2,1,6);
+VALUES('Figueirense','figueirense','png',30,'WPDXL3SGMTPQG',Now(),2,1,6);
