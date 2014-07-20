c6cd6786a9ea6938f7fd6b65fd7b98756ac7708d
viscardo <viscardo@gmail.com>
2014-07-20 20:15:18 -0300

    fancybox deu zica quando foi para o bundle

--- 30Fans.Web/App_Start/BundleConfig.cs
+++ 30Fans.Web/App_Start/BundleConfig.cs
@@ -20,10 +20,14 @@ namespace _30Fans.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/files").Include(
                       "~/Scripts/main.js",
                       "~/Scripts/editItem.js",
-                      "~/Scripts/jphotogrid.js"));
+                      "~/Scripts/jquery.fancybox.js",
+                      "~/Scripts/jquery.fancybox.pack.js"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- 30Fans.Web/Scripts/main.js
+++ 30Fans.Web/Scripts/main.js
@@ -87,21 +87,6 @@ $(document).ready(function() {
         friendFilter();
     });
 
-    $('#pg').jphotogrid({
-        baseCSS: {
-            width: '175px',
-            height: '117px',
-            padding: '0px'
-        },
-        selectedCSS: {
-            top: '50px',
-            left: '100px',
-            width: '500px',
-            height: '360px',
-            padding: '10px'
-        }
-    });
-
 
 });
 
